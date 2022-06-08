12cf515dae7a97a61687026e3559aa8128fb6e4f
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-08 19:03:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-user_token = 'vk1.a.c6soq1MfVq-Nr_8umTF-rW4OdzPS3O6XgbnWGvw6xz_U4ZTtSBb7fmiaT25oaGVh9RbpWAGc8EU4PF4Y90bpWDSDBtJ3WTft_pRUipMCWOFmsAyf1fkwau4gpgQ2QVygN1_x28IM8YHbusQ0ZtFziQ6VN9BMFKRVwyVqVhJTuSxvLxHfyVCifcsJoBIKo4QV'
-comm_token = '6a971f7e603945f1407b0d74c237993c9bfc7727a232534fc4efa2805db1f944ae86fc2542bc0c8adc3ed'
+user_token = ''
+comm_token = ''
