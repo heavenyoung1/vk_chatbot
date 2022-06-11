36fc0dba43c9d0b6a47c1fa99b29dd2ffc26dc2d
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-11 21:16:41 +0300

    update

--- bot.py
+++ bot.py
@@ -4,41 +4,47 @@ from config import user_token
 from config import comm_token
 from random import randrange
 import requests
+from main import *
+
 
 vk = vk_api.VkApi(token=comm_token) # Авторизуемся как сообщество
 longpoll = VkLongPoll(vk) # Работа с сообщениями
 
-
-def get_inform(user_id):
-    url = f'https://api.vk.com/method/users.get?user_ids={user_id}&fields=bdate,sex,city,relation&access_token={user_token}&v=5.131'
-    repl = requests.get(url)
-    response = repl.json()
-    information_dict = response['response']
-    for i in information_dict:
-        for key, value in i.items():
-            first_name = i.get('first_name')
-            last_name = i.get('last_name')
-    return first_name
-
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7),})
+    vk.method('messages.send', {'user_id': user_id,
+                                'message': message,
+                                'random_id': randrange(10 ** 7)
+                                })
+def photo_sender(user_id):
+    vk.method('message.send', {'user_id': user_id,
+                               'attachment' : 'photo' + str(get_photo(user_id)),
+                               'random_id': randrange(10 ** 7)})
 
 
 for event in longpoll.listen():
-    print('НАПИШИ')
     if event.type == VkEventType.MESSAGE_NEW: #Если пришло новое сообщение
         if event.to_me: #если оно имеет метку для меня(т.е бота)
             request = event.text  # Сообщение пользователя
+            user_id = str(event.user_id)
+            find_city(user_id)
+            get_photo(user_id)
+            #find_user(user_id
             if request == 'Привет' or request == 'привет':
-                write_msg(event.user_id, f'Привет, {get_inform("342034365")}')
+                write_msg(event.user_id, f'Привет, {name(user_id)}, твой id - {user_id}')
+                write_msg(event.user_id, get_sex(user_id))
+                #age_to()
+                #age_from()
+                photo_sender(user_id)
+                write_msg(event.user_id, city_id(user_id))
+                write_msg(user_id, get_photo(user_id))
+                #write_msg(event.user_id, get_age(user_id))
+                #write_msg(event.user_id, find_user(user_id))
+
             elif request == 'Пока':
                 write_msg(event.user_id, 'Пока.')
             else:
                 write_msg(event.user_id, 'Ваш вопрос непонятен.')
 
-
-def get_inform_1(self, user_id):
-    url = f'https://api.vk.com/method/users.get?user_ids={user_id}&fields=bdate,sex,city,relation&access_token={user_token}&v=5.131'
-    repl = requests.get(url)
-    response = repl.json()
-    information_dict = response['response']
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
