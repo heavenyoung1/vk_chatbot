e9c7da39a48600344338b55a574c954f041e7e6e
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-14 21:10:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ from config import comm_token
 from random import randrange
 import requests
 from main import *
-
-
 vk = vk_api.VkApi(token=comm_token) # Авторизуемся как сообщество
 longpoll = VkLongPoll(vk) # Работа с сообщениями
 
@@ -15,11 +13,11 @@ def write_msg(user_id, message):
                                 'message': message,
                                 'random_id': randrange(10 ** 7)
                                 })
+
 def photo_sender(user_id):
     vk.method('message.send', {'user_id': user_id,
                                'attachment' : 'photo' + str(get_photo(user_id)),
                                'random_id': randrange(10 ** 7)})
-
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW: #Если пришло новое сообщение
@@ -27,18 +25,17 @@ for event in longpoll.listen():
             request = event.text  # Сообщение пользователя
             user_id = str(event.user_id)
             find_city(user_id)
-            get_photo(user_id)
-            #find_user(user_id
+            #find_user(user_id)
             if request == 'Привет' or request == 'привет':
                 write_msg(event.user_id, f'Привет, {name(user_id)}, твой id - {user_id}')
-                write_msg(event.user_id, get_sex(user_id))
+                #write_msg(event.user_id, get_sex(user_id))
                 #age_to()
                 #age_from()
-                photo_sender(user_id)
+                send_photo(event.user_id, get_photo(user_id))
+                write_msg(event.user_id, get_photo(user_id))
                 write_msg(event.user_id, city_id(user_id))
-                write_msg(user_id, get_photo(user_id))
                 #write_msg(event.user_id, get_age(user_id))
-                #write_msg(event.user_id, find_user(user_id))
+                write_msg(event.user_id, find_user(user_id))
 
             elif request == 'Пока':
                 write_msg(event.user_id, 'Пока.')
