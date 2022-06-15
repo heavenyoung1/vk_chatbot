5204066875bd04d64340ddc9dd736ff6b05310a3
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-15 21:12:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from config import comm_token
 from random import randrange
 import requests
 from main import *
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
 vk = vk_api.VkApi(token=comm_token) # Авторизуемся как сообщество
 longpoll = VkLongPoll(vk) # Работа с сообщениями
 
@@ -13,28 +15,24 @@ def write_msg(user_id, message):
                                 'message': message,
                                 'random_id': randrange(10 ** 7)
                                 })
-
-def photo_sender(user_id):
-    vk.method('message.send', {'user_id': user_id,
-                               'attachment' : 'photo' + str(get_photo(user_id)),
-                               'random_id': randrange(10 ** 7)})
+def send_photo(user_id, url):
+    vk.method("messages.send", {"user_id": user_id,
+                                "message": "message",
+                                "attachment": 'photo',
+                                "random_id": 0})
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW: #Если пришло новое сообщение
         if event.to_me: #если оно имеет метку для меня(т.е бота)
-            request = event.text  # Сообщение пользователя
+            request = event.text.lower()  # Сообщение пользователя
             user_id = str(event.user_id)
             find_city(user_id)
             #find_user(user_id)
             if request == 'Привет' or request == 'привет':
+                keyboard(user_id, request)
                 write_msg(event.user_id, f'Привет, {name(user_id)}, твой id - {user_id}')
                 #write_msg(event.user_id, get_sex(user_id))
-                #age_to()
-                #age_from()
-                send_photo(event.user_id, get_photo(user_id))
-                write_msg(event.user_id, get_photo(user_id))
                 write_msg(event.user_id, city_id(user_id))
-                #write_msg(event.user_id, get_age(user_id))
                 write_msg(event.user_id, find_user(user_id))
 
             elif request == 'Пока':
@@ -45,5 +43,3 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
             request = event.text
-
-
