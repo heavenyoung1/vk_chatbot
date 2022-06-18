b9f07caa290584b2b909df49a29a7c03d5bd84a9
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-18 19:39:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,30 +15,33 @@ def write_msg(user_id, message):
                                 'message': message,
                                 'random_id': randrange(10 ** 7)
                                 })
-def send_photo(user_id, url):
+def send_photo(user_id, message):
     vk.method("messages.send", {"user_id": user_id,
-                                "message": "message",
-                                "attachment": 'photo',
+                                'access_token': user_token,
+                                'message': f'photo{user_id}_{get_photo_1(user_id)}',
+                                'attachment': f'photo{user_id}_{get_photo_1(user_id)}',
                                 "random_id": 0})
 
 for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW: #Если пришло новое сообщение
-        if event.to_me: #если оно имеет метку для меня(т.е бота)
-            request = event.text.lower()  # Сообщение пользователя
-            user_id = str(event.user_id)
-            find_city(user_id)
-            #find_user(user_id)
-            if request == 'Привет' or request == 'привет':
-                keyboard(user_id, request)
-                write_msg(event.user_id, f'Привет, {name(user_id)}, твой id - {user_id}')
-                #write_msg(event.user_id, get_sex(user_id))
-                write_msg(event.user_id, city_id(user_id))
-                write_msg(event.user_id, find_user(user_id))
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        request = event.text.lower()
+        user_id = str(event.user_id)
+        find_city(user_id)
+        get_photos_id(event.user_id)
+        if request == 'привет':
+            write_msg(event.user_id, f'Привет, {name(user_id)}') # , твой id - {user_id}
+            write_msg(event.user_id, get_sex(user_id))
+            write_msg(event.user_id, city_id(user_id))
+            get_age(event.user_id)
+            #write_msg(event.user_id, get_age(user_id))
+            #send_photo(user_id, 'mmm')
+            #write_msg(event.user_id, find_user(user_id))
 
-            elif request == 'Пока':
-                write_msg(event.user_id, 'Пока.')
-            else:
-                write_msg(event.user_id, 'Ваш вопрос непонятен.')
+        elif request == 'Пока':
+            write_msg(event.user_id, 'Пока.')
+        else:
+            write_msg(event.user_id, 'Ваш вопрос непонятен.')
+
 
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
