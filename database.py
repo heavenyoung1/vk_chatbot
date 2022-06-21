05b74cf9d4f385609ec5348501c5e85125a6e41f
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-21 21:55:43 +0300

    Update database.py

--- database.py
+++ database.py
@@ -11,23 +11,39 @@ connection = psycopg2.connect(
 
 connection.autocommit = True
 
-
 def create_table_users():
     with connection.cursor() as cursor:
         cursor.execute(
             """CREATE TABLE users(
                 id serial,
                 first_name varchar(50) NOT NULL,
-                last_name varchar(50) NOT NULL,
-                vk_id varchar(75) NOT NULL PRIMARY KEY,
-                vk_link varchar(75));"""
+                last_name varchar(25) NOT NULL,
+                vk_id varchar(20) NOT NULL PRIMARY KEY,
+                vk_link varchar(50));"""
         )
-    print("[INFO] Table was created.")
+    print("[INFO] Table USERS was created.")
 
-#def seen_users():
-    
-    
-    
+# def create_table_seen_users():
+#     with connection.cursor() as cursor:
+#         cursor.execute(
+#             """CREATE TABLE seen_users(
+#             id serial,
+#             vk_id varchar(50) PRIMARY KEY references users(vk_id));"""
+#         )
+
+# def insert_seen_users(vk_id):
+#      with connection.cursor() as cursor:
+#             cursor.execute(
+#                 f"""INSERT INTO seen_users (vk_id) VALUES ('{found_vk_id(offset)}');""")
+
+# def drop_seen_users():
+#     with connection.cursor() as cursor:
+#         cursor.execute(
+#             """DROP TABLE seen_users;"""
+#         )
+#         print('[INFO] Table SEEN_USERS was deleted.')
+
+
 
 # def insert_data():
 #     with connection.cursor() as cursor:
@@ -39,8 +55,8 @@ def create_table_users():
 def insert_data(first_name, last_name, vk_id, vk_link):
     with connection.cursor() as cursor:
         cursor.execute(
-            f"""INSERT INTO users (first_name, last_name, vk_id, vk_link) VALUES
-            ('{first_name}', '{last_name}', '{vk_id}', '{vk_link}');"""
+            f"""INSERT INTO users (first_name, last_name, vk_id, vk_link) 
+            VALUES ('{first_name}', '{last_name}', '{vk_id}', '{vk_link}');"""
         )
 
 # def select():
@@ -50,10 +66,19 @@ def insert_data(first_name, last_name, vk_id, vk_link):
 #         )
 #         return cursor.fetchone()
 
+# def select(offset):
+#     with connection.cursor() as cursor:
+#         cursor.execute(
+#             f"""SELECT first_name, last_name, vk_id, vk_link  FROM users
+#                 ORDER BY id
+#                 OFFSET '{offset}';"""
+#         )
+#         return cursor.fetchone()
+
 def select(offset):
     with connection.cursor() as cursor:
         cursor.execute(
-            f"""SELECT first_name, last_name, vk_id, vk_link  FROM users 
+            f"""SELECT first_name, last_name, vk_id, vk_link  FROM users
                 ORDER BY id
                 OFFSET '{offset}';"""
         )
@@ -65,6 +90,13 @@ def found_person_info(offset):
     for i in tuple:
         list.append(i)
     return f'{list[0]} {list[1]}, ссылка - {list[3]}'
+
+def found_vk_id(offset):
+    tuple = select(offset)
+    list = []
+    for i in tuple:
+        list.append(i)
+    return f'{list[2]}'
 
 def person_id(offset):
     tuple = select(offset)
@@ -79,11 +111,11 @@ def drop():
         cursor.execute(
             """DROP TABLE users;"""
         )
-        print('[INFO] Table was deleted.')
+        print('[INFO] Table USERS was deleted.')
 
 # drop()
 # create_table()
 # insert_data()
 # drop()
 #(select(50))
-
+#drop_seen_users()
