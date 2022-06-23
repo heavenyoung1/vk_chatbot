f1b22029ce3e68629535350f46b0f75ef28fb643
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-23 17:52:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-user_token = ''
-comm_token = ''
+user_token = '' # токен пользователя
+comm_token = '' # токен сообщества
 
-host = '127.0.0.1'
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
 user = 'postgres'
 password = '0000'
 db_name = 'bot_users'
