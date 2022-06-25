ee6743ebaaf914928027938b3c641100686a44d2
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-06-25 13:03:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,40 +4,6 @@ from config import user_token, comm_token, offset, line
 from main import *
 
 
-def send_photo_1(user_id, message):
-    vk.method("messages.send", {"user_id": user_id,
-                                'access_token': user_token,
-                                'message': message,
-                                'attachment': f'photo{person_id(offset)}_{get_photo_1(person_id(offset))}',
-                                "random_id": 0})
-
-def send_photo_2(user_id, message):
-    vk.method("messages.send", {"user_id": user_id,
-                                'access_token': user_token,
-                                'message': message,
-                                'attachment': f'photo{person_id(offset)}_{get_photo_2(person_id(offset))}',
-                                "random_id": 0})
-
-def send_photo_3(user_id, message):
-    vk.method("messages.send", {"user_id": user_id,
-                                'access_token': user_token,
-                                'message': message,
-                                'attachment': f'photo{person_id(offset)}_{get_photo_3(person_id(offset))}',
-                                "random_id": 0})
-
-def find_persons(user_id, offset):
-    write_msg(user_id, found_person_info(offset))
-    person_id(offset)
-    insert_data_seen_users(person_id(offset), offset )
-    get_photos_id(person_id(offset))
-    send_photo_1(user_id, 'Фото номер 1')
-    if get_photo_2(person_id(offset)) != None:
-        send_photo_2(user_id, 'Фото номер 2')
-        send_photo_3(user_id, 'Фото номер 3')
-    else:
-        write_msg(user_id, f'Больше фотографий нет')
-
-
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         request = event.text.lower()
